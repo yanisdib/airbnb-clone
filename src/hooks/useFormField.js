4ff11653f2570a2b2c5beddf7425bd38edfe1b70
Yanis Dib <ynsprive@gmail.com>
2022-01-25 16:47:19 +0100

    Create hook for Form fields

---
+++ src/hooks/useFormField.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+export default (initialValue) => {
+    const [value, setValue] = useState(initialValue);
+
+    const updateValue = (event) => {
+        const targetType = event.target.type;
+        
+        setValue(targetType === 'checkbox' ? event.target.checked : event.target.value);
+    };
+
+    return [value, updateValue];
+}
