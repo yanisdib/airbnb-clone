2e1dd7ed65484dcca475daff6f1253585febdb0a
Yanis Dib <ynsprive@gmail.com>
2022-01-26 00:28:21 +0100

    Update CSS variables and properties

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.div`
-    --container-max-width-xs: 1760px;
-    --container-max-width-sm: 1760px;
-    --container-max-width-md: 1760px;
-    --container-max-width-lg: 1760px;
+    --container-max-width-xs: 100vw;
+    --container-max-width-sm: 100vw;
+    --container-max-width-md: 960px;
+    --container-max-width-lg: 1360px;
     --container-max-width-xl: 1760px;
 
     --container-padding-x: 80px !important;
@@ -12,18 +12,30 @@ export default styled.div`
 
     @media (min-width: 375px) {
         display: flex;
+        width: 100%;
+        max-width: var(--container-max-width-xs);
+        padding-left: 0;
+        padding-right: 0;
     }
 
     @media (min-width: 744px) {
         display: flex;
+        width: 100%;
+        max-width: var(--container-max-width-sm);
+        padding-left: 0;
+        padding-right: 0;
     }
 
     @media (min-width: 950px) {
         display: flex;
+        width: 91.666666666667vw;
+        max-width: var(--container-max-width-md);
     }
 
     @media (min-width: 1128px) {
         display: flex;
+        width: 91.666666666667vw;
+        max-width: var(--container-max-width-lg);
     }
 
     @media (min-width: 1440px) {
