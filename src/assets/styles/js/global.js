a047eb748f1e1863d704efdd28ef4389506cf1e6
Yanis Dib <ynsprive@gmail.com>
2022-01-25 16:49:33 +0100

    Add global styles to project

---
+++ src/assets/styles/js/global.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export default createGlobalStyle`
+    *,
+    *::after,
+    *::before {
+        margin: 0;
+        padding: 0;
+        border: none;
+        list-style-type: none;
+        box-sizing: border-box; 
+    }
+
+    :root {
+        /** COLORS */
+        --brand-color: #FF385C;
+        --ui-brand-color: #000000; 
+
+        /** BREAKPOINTS */
+        --ui-media-breakpoints-xs: 1440px;
+        --ui-media-breakpoints-sm: 744px;
+        --ui-media-breakpoints-md: 950px;
+        --ui-media-breakpoints-lg: 1128px;
+        --ui-media-breakpoints-xl: 1440px;
+
+        /** GUTTERS */
+        --ui-padding-x: 10px;
+        --ui-padding-y: 15px;
+    }
+
+    body {
+        background: ${({ theme }) => theme.body};
+        color: ${({ theme }) => theme.text};
+        transition: background color 0.25s linear;
+        font-family: "Poppins", sans-serif;
+        font-weight: 300;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button {
+        font-family: inherit;
+        color: ${({ theme }) => theme.text};
+    }
+`;
---
+++ src/assets/styles/js/theme.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+export const theme = {
+    body: '#FFFFFF',
+    text: '#000000',
+    gradient: 'linear-gradient(#091236, #1E215D)'
+};
