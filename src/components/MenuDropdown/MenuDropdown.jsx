8ed16a9e0834102834cd6448556d65e714f45f11
Yanis Dib <ynsprive@gmail.com>
2022-01-27 23:15:02 +0100

    Update styles

--- src/components/MenuDropdown/MenuDropdown.jsx
+++ src/components/MenuDropdown/MenuDropdown.jsx
@@ -6,6 +6,7 @@ import { Dropdown } from '..';
 const Wrapper = styled.div`
     width: 100%;
     height: 100%;
+    margin-left: 0 !important;
 `;
 
 const AuthLinkList = styled.ul`
@@ -29,12 +30,26 @@ const AuthLinkList = styled.ul`
 
 const PublicLinkList = styled.ul`
     padding: 7px 0;
+
+    a {
+        display: flex;
+        width: fit-content;
+        color: #000000;
+        font-size: 0.9em;
+        font-weight: 400;
+        padding: 10px 16px;
+        text-decoration: none;
+
+        &:first-child {
+            font-weight: 500;
+        }
+    }
 `;
 
 
 function MenuDropdown() {
     return (
-        <Dropdown>
+        <Dropdown width={46}>
             <Wrapper>
                 <AuthLinkList>
                     <a href="/">Sign up</a>
