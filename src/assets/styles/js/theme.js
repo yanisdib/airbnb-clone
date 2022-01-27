76bdd59689c7e851df9e549ed95f36219ad93e80
Yanis Dib <ynsprive@gmail.com>
2022-01-27 23:14:50 +0100

    Remove useless styles

--- src/assets/styles/js/theme.js
+++ src/assets/styles/js/theme.js
@@ -1,8 +1,4 @@
-import styled from 'styled-components';
-
-
 export const theme = {
     body: '#FFFFFF',
-    text: '#000000',
-    gradient: 'linear-gradient(#091236, #1E215D)'
+    text: '#000000'
 };
