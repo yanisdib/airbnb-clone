952f58bd671d42a6f653d1d9498a47d6186abfbf
Yanis Dib <ynsprive@gmail.com>
2022-01-27 23:11:28 +0100

    Update styles for responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,19 +6,55 @@ import bannerImage from '../../assets/images/flexible-banner.jpg';
 
 
 const HeroSection = styled.section`
+    --sc-spt-xs: 0px;
+    --sc-spb-xs: 0px;
+    --sc-spt-sm: 0px;
+    --sc-spb-sm: 0px;
+    --sc-spt-md: 230px;
+    --sc-spb-md: 0px;
+    --sc-spt-lg: 176px;
+    --sc-spb-lg: 96px;
+    --sc-spt-xl: 176px;
+    --sc-spb-xl: 96px;
+
+    --banner-min-height: 50%;
+
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 92vh;
+    height: auto;
     --hero-text-color: #FFFFFF; 
     background-color: var(--ui-brand-color);
+
 `;
 
 const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    height: inherit;
+    @media (min-width: 375px) {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        padding-top: var(--sc-spt-xs);
+        padding-bottom: var(--sc-spt-xs);
+    }
+    @media (min-width: 744px) {
+        padding-top: var(--sc-spt-sm);
+        padding-bottom: var(--sc-spt-sm)
+    }    
+    
+    @media (min-width: 950px) {
+        padding-top: var(--sc-spt-md);
+        padding-bottom: var(--sc-spt-md)
+    }
+
+    @media (min-width: 1128px) {
+        padding-top: var(--sc-spt-lg);
+        padding-bottom: var(--sc-spt-lg)
+    }
+
+    @media (min-width: 1440px) {
+        padding-top: var(--sc-spt-xl);
+        padding-bottom: var(--sc-spt-xl)
+    }
 `;
 
 const Content = styled.div`
@@ -30,6 +66,7 @@ const Content = styled.div`
         align-items: center;
         width: inherit;
         height: 100%;
+        min-height: 800px;
         max-height: 800px;
         background-image: url(${bannerImage});
         background-position: center;
@@ -46,6 +83,8 @@ const Content = styled.div`
     @media (min-width: 1128px) {
         border-radius: 12px;
     }
+
+    
 `;
 
 const Heading = styled.h2`
