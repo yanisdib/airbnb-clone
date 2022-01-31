35d7e04d094ab468ae522371371e86712f07e1ee
Yanis Dib <ynsprive@gmail.com>
2022-01-31 20:49:17 +0100

    Add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 export { default as Container } from './Container/Container';
 export { default as Dropdown } from './Dropdown/Dropdown';
 
+export { default as SearchBar } from './SearchBar/SearchBar';
+
 export { default as MenuButton } from './MenuButton/MenuButton';
 export { default as MenuDropdown } from './MenuDropdown/MenuDropdown';
 
