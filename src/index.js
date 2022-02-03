69ab0a64f1099e5605011dde09319d41e327d4cf
Yanis Dib <ynsprive@gmail.com>
2022-02-03 14:27:44 +0100

    Add store and Redux provider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+
+import reportWebVitals from './reportWebVitals';
+import configureStore from './services/redux/store/configureStore';
 
 import App from './App';
 
-import reportWebVitals from './reportWebVitals';
-
 import GlobalStyle from './assets/styles/js/global';
 import { theme } from './assets/styles/js/theme';
+
+
+const store = configureStore();
 
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
