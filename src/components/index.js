ed02c22e44087e56a72f3d282e7b8d3c3eded36a
Yanis Dib <ynsprive@gmail.com>
2022-02-12 00:39:21 +0100

    Add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 export { default as Container } from './Container/Container';
 export { default as Dropdown } from './Dropdown/Dropdown';
+export { default as CardGrid } from './CardGrid/CardGrid';
 
 export { default as SearchBar } from './SearchBar/SearchBar';
+export { default as SearchBarButton } from './SearchBarButton/SearchBarButton';
 
 export { default as MenuButton } from './MenuButton/MenuButton';
 export { default as MenuDropdown } from './MenuDropdown/MenuDropdown';
@@ -9,3 +11,5 @@ export { default as MenuDropdown } from './MenuDropdown/MenuDropdown';
 export { default as Header } from './Header/Header';
 
 export { default as Hero } from './Hero/Hero';
+
+export { default as TripSuggestion } from './TripSuggestion/TripSuggestion';
