4918981829a468961517b86033d04eb9181fef7a
Yanis Dib <ynsprive@gmail.com>
2022-02-12 19:16:39 +0100

    Add new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Container } from './Container/Container';
+export { default as Button } from './Button/Button';
 export { default as Dropdown } from './Dropdown/Dropdown';
 export { default as CardGrid } from './CardGrid/CardGrid';
 
@@ -13,3 +14,4 @@ export { default as Header } from './Header/Header';
 export { default as Hero } from './Hero/Hero';
 
 export { default as TripSuggestion } from './TripSuggestion/TripSuggestion';
+export { default as ExperiencesSection } from './ExperiencesSection/ExperienceSection';
