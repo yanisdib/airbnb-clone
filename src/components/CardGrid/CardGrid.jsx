25d30051bd475c50e2598697c3d28389bbee5ac9
Yanis Dib <ynsprive@gmail.com>
2022-02-14 16:27:06 +0100

    Add responsiveness

--- src/components/CardGrid/CardGrid.jsx
+++ src/components/CardGrid/CardGrid.jsx
@@ -2,8 +2,27 @@ import styled from 'styled-components';
 
 
 export default styled.div`
-    display: grid;
-    grid-template-columns: 382px 382px 382px 382px;
-    row-gap: 24px;
-    column-gap: 24px;
+    @media (min-width: 375px) {
+        display: grid;
+        justify-content: center;
+        grid-template-columns: 1fr 1fr;
+        row-gap: 24px;
+        column-gap: 24px;
+    }
+
+    @media (min-width: 744px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 950px) {
+        grid-template-columns: auto auto auto;
+    }
+
+    @media (min-width: 1128px) {
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+
+    @media (min-width: 1440px) {
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
 `;
